4d1c6953dc2abad1eb40549d9e93a7f59bdb5780
htn190007 <72238948+htn190007@users.noreply.github.com>
2025-03-04 23:13:20 -0600

    backend and api

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js';
+
+import productRoutes from "./routes/product.route.js"
+
 dotenv.config()
 
 const app = express();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.post("/api/products", async (req,res) =>{
-    const product = req.body; // user will send this data
-
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({success:false, message: "Please provide all fields"});
-    }
-
-    const newProduct = new Product(product)
-
-    try {
-        await newProduct.save();
-        res.status(201).json({success:true, data:newProduct});
-    } catch (error) {
-        console.error("Error in Create product:", error.message);
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
-
-
+app.use("/api/products", productRoutes)
 
 app.listen(5000, () =>{
     connectDB();
