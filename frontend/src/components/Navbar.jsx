2a920b1232668e75cbadd4e81c2b8bac230ff075
htn190007 <72238948+htn190007@users.noreply.github.com>
2025-03-05 02:24:41 -0600

    frontend home page and create page, finished create product, need to add update, get all and delete product

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { Container, Flex, Text, Link, HStack, Button} from '@chakra-ui/react'
+import React from 'react'
+import { FaRegPlusSquare } from "react-icons/fa";
+import { useColorMode } from './ui/color-mode';
+import { LuMoon, LuSun } from "react-icons/lu"
+
+const Navbar = () => {
+  const { toggleColorMode } = useColorMode()
+
+  return (
+    <Container maxW={"1140px"} px={4} >
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base:"column",
+          sm:"row"
+        }}
+        >
+          <Text
+            fontSize={{base: "20px", sm: "28px"}}
+            fontWeight={"bold"}
+            textTransform={"uppercase"}
+            textAlign={"center"}
+          >
+            <Link href={"/"}>Product Store 🛒</Link>
+          </Text>
+
+          <HStack spacing={2} alignItems={"center"}>
+            <Link href={"/create"}>
+              <Button>
+                <FaRegPlusSquare fontSize={20}></FaRegPlusSquare>
+              </Button>
+            </Link>
+            <Button onClick={toggleColorMode}>
+              <LuSun /><LuMoon />
+            </Button>
+          </HStack>
+      </Flex>
+    </Container>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,64 @@
+
+import { useColorModeValue } from '../components/ui/color-mode'
+import { Box, Button, Container, Heading, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { useProductStore } from '../../store/product'
+
+const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: ""
+  })
+
+  const {createProduct} = useProductStore()
+  const handleAddProduct = async() => {
+    console.log(newProduct)
+    const {success, message} = await createProduct(newProduct)
+    console.log("Success:", success)
+    console.log("message:", message)
+  }
+  
+  return (
+    <Container maxW = {"sm"}>
+      <VStack
+        spacing={8}
+      >
+        <Heading as="h1" size="4xl" textAlign={"center"} mb={8}>
+          Create new Product
+        </Heading>
+        <Box
+          w={"full"} bg={useColorModeValue("white","gray.800")}
+          p={6} rounded={"lg"} shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder='Product Name'
+              name='name'
+              value={newProduct.name}
+              onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value})}
+              />
+            <Input
+              placeholder='Price'
+              name='price'
+              value={newProduct.price}
+              onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value})}
+              />
+            <Input
+              placeholder='Image URL'
+              name='image'
+              value={newProduct.image}
+              onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value})}
+              />
+            
+            <Button colorPalette={'blue'} onClick={handleAddProduct} w='full'>Add Product</Button>
+
+          </VStack>
+
+        </Box>
+      </VStack>
+    </Container>
+  )
+}
+
+export default CreatePage
